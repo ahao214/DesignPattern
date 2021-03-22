790312e354d59094b321e33fa4a5c699d4eeb023
ahao214 <haochen214@163.com>
2021-03-22 20:49:08 +0800

    抽象图表接口

---
+++ SimpleFactorySample/IChart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactorySample
+{
+    /// <summary>
+    /// Chart:抽象图表接口，充当抽象产品类
+    /// </summary>
+    interface IChart
+    {
+        void Display();
+    }
+}
