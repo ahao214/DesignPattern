0f2bd4c7480f2c8df12009766f607056178bc539
ahao214 <haochen214@163.com>
2021-03-25 21:27:25 +0800

    静态工厂和测试类

---
+++ SimpleFactorySample/ChartFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactorySample
+{
+    /// <summary>
+    /// 图表工厂类，充当工厂类
+    /// </summary>
+    class ChartFactory
+    {
+        //静态工厂方法
+        public static IChart GetChart(string type)
+        {
+            IChart chart = null;
+            if (type.Equals("histogram"))
+            {
+                chart = new HistogramChart();
+                Console.WriteLine("初始化设置柱状图");
+            }
+            else if (type.Equals("pie"))
+            {
+                chart = new PieChart();
+                Console.WriteLine("初始化设置饼状图");
+            }
+            else if (type.Equals("line"))
+            {
+                chart = new LineChart();
+                Console.WriteLine("初始化设置折线图");
+            }
+            return chart;
+        }
+
+    }
+}
--- SimpleFactorySample/Program.cs
+++ SimpleFactorySample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,14 @@ namespace SimpleFactorySample
     {
         static void Main(string[] args)
         {
+            IChart chart;
+            //读取配置文件
+            string chartStr = ConfigurationManager.AppSettings["chartType"];
+            //通过静态工厂方法创建产品
+            chart = ChartFactory.GetChart(chartStr);
+            chart.Display();
+
+            Console.ReadLine();
         }
     }
 }
