a3a2956962d8c335e42d703d5b335b9ef3c492b3
ahao214 <haochen214@163.com>
2021-03-26 22:59:31 +0800

    工厂方法模式

---
+++ FactoryMethodSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodSample
+{
+    class Program
+    {
+        /// <summary>
+        /// 工厂方法模式
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
