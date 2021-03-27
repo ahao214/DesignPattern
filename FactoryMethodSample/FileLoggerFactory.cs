461394fb0ad1ee7478ba99404729a86a12c63ae3
ahao214 <haochen214@163.com>
2021-03-27 11:21:08 +0800

    日志记录器工厂

---
+++ FactoryMethodSample/FileLoggerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodSample
+{
+    /// <summary>
+    /// 文件日志记录器工厂类，充当具体工厂角色
+    /// </summary>
+    class FileLoggerFactory:LoggerFactory
+    {
+        public Logger CreateLogger()
+        {
+            //创建文件日志记录器对象
+            Logger logger = new FileLogger();
+            return logger;
+        }
+    }
+}
