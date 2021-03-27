2008919acdf42b65a0900592c5a87ed528f38d14
ahao214 <haochen214@163.com>
2021-03-27 11:30:08 +0800

    工厂方法模式

--- FactoryMethodSample/Program.cs
+++ FactoryMethodSample/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
 
 namespace FactoryMethodSample
 {
@@ -14,6 +12,25 @@ namespace FactoryMethodSample
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            LoggerFactory factory;  //针对抽象工厂类编程
+            Logger logger;              //针对抽象产品类编程
+            factory = new DatabaseLoggerFactory();
+            logger = factory.CreateLogger();
+            logger.WriteLog();
+
+            #region 使用反射
+
+            string factoryString = ConfigurationManager.AppSettings["factory"];
+            //反射生成对象
+            factory = (LoggerFactory)Assembly.Load("FactoryMethodSample").CreateInstance(factoryString);
+            logger = factory.CreateLogger();
+            logger.WriteLog();
+
+            #endregion 
+
+
+            Console.ReadLine();
+
         }
     }
 }
