694041c33fb76679733add877249ece2e354201f
ahao214 <haochen214@163.com>
2021-03-28 11:45:39 +0800

    抽象工厂抽象产品

---
+++ AbstractFactorySample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactorySample
+{
+    class Program
+    {
+        /// <summary>
+        /// 抽象工厂模式
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
