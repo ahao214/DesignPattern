4829c63d4530367cefb06d29b1f1e1f9973bacc4
ahao214 <haochen214@163.com>
2021-03-28 12:03:59 +0800

    实现抽象工厂方法

--- AbstractFactorySample/Program.cs
+++ AbstractFactorySample/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
 
 namespace AbstractFactorySample
 {
@@ -14,6 +16,27 @@ namespace AbstractFactorySample
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //使用抽象层定义
+            SkinFactory factory;
+            Button bt;
+            TextField tf;
+            ComboBox cb;
+
+            //读取配置文件
+            string factoryType = ConfigurationManager.AppSettings["factory"];
+
+            //反射生成对象
+            factory = (SkinFactory)Assembly.Load("AbstractFactorySample").CreateInstance(factoryType);
+
+            bt = factory.CreateButton();
+            tf = factory.CreateTextField();
+            cb = factory.CreateComboBox();
+            bt.Display();
+            tf.Display();
+            cb.Display();
+
+            Console.Read();
+
         }
     }
 }
