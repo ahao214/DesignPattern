c890b5eef416890c7e7db3e547d8ef1394185bd5
liupenghao@fulan.com.cn <haochen214@163.com>
2023-01-15 20:02:20 +0800

    原型模式

---
+++ PrototypeSample/Attachment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeSample
+{
+    /// <summary>
+    /// 附件类
+    /// </summary>
+    public class Attachment
+    {
+        public string name { get; set; }
+
+        public void Download()
+        {
+            Console.WriteLine("下载附件,文件名为：{0}", name);
+        }
+    }
+}
---
+++ PrototypeSample/WeeklyLog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeSample
+{
+    /// <summary>
+    /// 周报类：充当原型角色
+    /// </summary>
+    public class WeeklyLog
+    {
+        private Attachment attachment;
+        private string name;
+        private string date;
+        private string content;
+
+        public Attachment Attachment
+        {
+            get { return attachment; }
+            set { attachment = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        //使用MemberwiseClone()方法实现浅克隆
+        public WeeklyLog Clone()
+        {
+            return this.MemberwiseClone() as WeeklyLog;     //类型转换
+        }
+
+    }
+}
